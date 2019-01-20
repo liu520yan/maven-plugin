f27a31b9727f461f67eb044c52145a5b2267fc4f
liu520yan <1027552153@qq.com>
2019-01-20 23:57:19 +0800

    plugin

--- src/main/java/com/plugin/test/MyMojo.java
+++ src/main/java/com/plugin/test/MyMojo.java
@@ -6,26 +6,80 @@ import org.apache.maven.plugins.annotations.LifecyclePhase;
 import org.apache.maven.plugins.annotations.Mojo;
 import org.apache.maven.plugins.annotations.Parameter;
 
+import java.io.*;
 
-@Mojo(name = "sayhi", defaultPhase = LifecyclePhase.COMPILE)
+
+@Mojo(name = "aopparamchange", defaultPhase = LifecyclePhase.COMPILE)
 public class MyMojo extends AbstractMojo {
 
-    @Parameter
-    private String age;
+    @Parameter(property = "filePath")
+    private String filePath;
+
+    @Parameter(property = "source")
+    private String source;
+
+    @Parameter(property = "target")
+    private String target;
 
     public void execute() throws MojoExecutionException {
-        System.out.println("ok le ....");
+        File file = new File(filePath);
+        if (!file.isFile()) {
+            throw new MojoExecutionException(filePath + "not a file");
+        }
 
-        System.out.println("age：" + age);
+        try {
+            BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file), "UTF-8"));
+
+            //内存流
+            CharArrayWriter caw = new CharArrayWriter();
+
+            //替换
+            String line = null;
+
+            //以行为单位进行遍历
+            while ((line = br.readLine()) != null) {
+                //替换每一行中符合被替换字符条件的字符串
+                line = line.replaceAll(source, target);
+                //将该行写入内存
+                caw.write(line);
+                //添加换行符，并进入下次循环
+                caw.append(System.getProperty("line.separator"));
+            }
+            //关闭输入流
+            br.close();
+
+            //将内存中的流写入源文件
+            FileWriter fw = new FileWriter(file);
+            caw.writeTo(fw);
+            fw.close();
+        } catch (Exception e) {
+            throw new MojoExecutionException(e.getMessage());
+        }
 
     }
 
-    public String getAge() {
-        return age;
+    public String getFilePath() {
+        return filePath;
     }
 
-    public void setAge(String age) {
-        this.age = age;
+    public void setFilePath(String filePath) {
+        this.filePath = filePath;
+    }
+
+    public String getSource() {
+        return source;
+    }
+
+    public void setSource(String source) {
+        this.source = source;
+    }
+
+    public String getTarget() {
+        return target;
+    }
+
+    public void setTarget(String target) {
+        this.target = target;
     }
 }
     
